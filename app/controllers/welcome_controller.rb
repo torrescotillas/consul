31c70d3266fc8475eabb45df37486f4098a55ff9
Torres de Cotillas <ja.cabello@ticsmart.com>
2023-11-23 13:53:14 +0100

    fix(design): several fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,8 +12,8 @@ class WelcomeController < ApplicationController
     @feeds = Widget::Feed.active
     @cards = Widget::Card.body
 
-    # Obtén los 8 debates limitados
-    @limited_debates = Debate.limit(5)
+    # Obtén los 5 debates limitados
+    @limited_debates = Debate.where(hidden_at: nil).limit(5)
     @limited_proposals = Proposal.where.not(published_at: nil, selected: true).where(hidden_at: nil).limit(5).order(id: :asc)
 
     
